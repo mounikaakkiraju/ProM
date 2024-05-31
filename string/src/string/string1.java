a2bdf0a344d9c8771dbd69a599ec9879ef0b5589
mouni <mouni@mounika>
2024-05-31 16:02:58 +0530

    push

---
+++ string/src/string/string1.java
@@ -0,0 +1,29 @@
+package string;
+
+public class string1 {
+	  public static int abc(int x,int y)
+	  {
+		  return x+y;
+	  }
+	  public static int xyz(int x,int y) 
+	  {
+		 return x*y; 
+	  }
+	  public static int pqr(int x,int y)
+	  {
+		  return x%y;
+	  }
+	  
+	  public static void main(String[] args) 
+	  {
+      int result = abc(4,2);
+      System.out.println("the reult is :"+result);
+      int result2 = xyz(5,2); 
+      System.out.println("the result is :"+result2);
+      int result3=pqr(6,4);
+      System.out.println("the result is :"+result3);
+	  }
+	  
+	  
+
+}
